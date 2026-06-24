527bd8d29ab193556fa8c3e9172039c85497f7a9
Lorenzo Antonio Bonani Canales <lorenzoantoniobonanicanales@MacBook-Pro-de-Lorenzo.local>
2026-06-24 10:19:05 -0400

    Conectar frontend a API Railway: login, registro y sesión persistente

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bucaly-v13';
+const CACHE_NAME = 'bucaly-v14';
 const urlsToCache = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
