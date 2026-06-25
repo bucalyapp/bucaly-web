6f16224fc71b58cd810bc17b8661b12259350573
Lorenzo Antonio Bonani Canales <lorenzoantoniobonanicanales@MacBook-Pro-de-Lorenzo.local>
2026-06-25 07:35:33 -0400

    Fix: IDs SVG duplicados en dashboard + bump SW v20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bucaly-v19';
+const CACHE_NAME = 'bucaly-v20';
 const urlsToCache = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
