849f4399750e8bc8d11b59c363d191d7d2a05be3
Lorenzo Antonio Bonani Canales <lorenzoantoniobonanicanales@MacBook-Pro-de-Lorenzo.local>
2026-07-15 08:33:36 -0400

    fix: botón Mi Portal mismo tamaño que Iniciar triaje

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bucaly-v27';
+const CACHE_NAME = 'bucaly-v28';
 const urlsToCache = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
