cfdd690e1b0c8f8eed8165ed6c66bdd4fc12e985
Lorenzo Antonio Bonani Canales <lorenzoantoniobonanicanales@MacBook-Pro-de-Lorenzo.local>
2026-08-17 13:32:11 -0400

    fix: remove stale buildSwipeFlow call causing JS crash on load

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bucaly-v47';
+const CACHE_NAME = 'bucaly-v48';
 const urlsToCache = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
