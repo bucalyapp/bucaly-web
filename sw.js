0a8876842aa536e9b316f91b2599d642b2af3f3b
Lorenzo Antonio Bonani Canales <lorenzoantoniobonanicanales@MacBook-Pro-de-Lorenzo.local>
2026-08-17 12:59:21 -0400

    fix: SW updateViaCache none + bump v44 para forzar update en iOS

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'bucaly-v43';
+const CACHE_NAME = 'bucaly-v44';
 const urlsToCache = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', event => {
